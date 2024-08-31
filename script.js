3ea5f0f8f30b56a422a3a232ab1ea50e192ccf50
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2024-08-31 12:56:58 +0300

    Added Contact Form.
    Added Contact Form. Scrolling behaviour changed. Users recieve Email with offers

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.addEventListener("scroll", function () {
 });
 document.addEventListener("scroll", function () {
     const sections = document.querySelectorAll(".who");
-    const fadeStart = 250; 
+    const fadeStart = 150; 
     const fadeEnd = 500;   
 
     sections.forEach(section => {
@@ -179,6 +179,54 @@ document.addEventListener("scroll", function () {
     const sections = document.querySelectorAll("#link4");
     const fadeStart = 500; 
     const fadeEnd = 800;   
+
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const elementTop = rect.top + scrollTop;
+
+        let opacity = 1;
+
+        if (scrollTop > elementTop - fadeStart) {
+            const fadeDistance = fadeEnd - fadeStart;
+            opacity = 1 - (scrollTop - (elementTop - fadeStart)) / fadeDistance;
+
+            if (opacity < 0) {
+                opacity = 0;
+            }
+        }
+
+        section.style.opacity = opacity;
+    });
+});
+document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll(".services");
+    const fadeStart = 150; 
+    const fadeEnd = 500;   
+
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        const elementTop = rect.top + scrollTop;
+
+        let opacity = 1;
+
+        if (scrollTop > elementTop - fadeStart) {
+            const fadeDistance = fadeEnd - fadeStart;
+            opacity = 1 - (scrollTop - (elementTop - fadeStart)) / fadeDistance;
+
+            if (opacity < 0) {
+                opacity = 0;
+            }
+        }
+
+        section.style.opacity = opacity;
+    });
+});
+document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll(".simages");
+    const fadeStart = 250; 
+    const fadeEnd = 1000;   
 
     sections.forEach(section => {
         const rect = section.getBoundingClientRect();
